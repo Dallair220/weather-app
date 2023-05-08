70f40f9ea97e32d787e6ed704f43c1e68283664b
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-05-08 20:51:56 +0200

    Add display of city weather data and forecast

--- src/displayUI.js
+++ src/displayUI.js
@@ -1,6 +1,50 @@
-export default function display(city) {
-  const container = document.querySelector('.container');
+const dataContainer = document.querySelector('.data');
+
+function clearDisplay() {
+  dataContainer.innerHTML = '';
+}
+
+function addCityData(city, country, localtime) {
   const title = document.createElement('h1');
-  title.textContent = 'something';
-  container.appendChild(title);
+  title.textContent = `${city}, ${country}`;
+  dataContainer.appendChild(title);
+
+  const time = document.createElement('h5');
+  time.textContent = localtime;
+  dataContainer.appendChild(time);
+}
+
+function addCurrentData(condition, temperature, sunrise, sunset) {
+  const tempCond = document.createElement('h3');
+  tempCond.textContent = `TODAY: ${temperature}°C, ${condition}`;
+  dataContainer.appendChild(tempCond);
+
+  const sun = document.createElement('h5');
+  sun.textContent = `Sunrise: ${sunrise}  | Sunset: ${sunset}`;
+  dataContainer.appendChild(sun);
+}
+
+function addForeCastData(forecast) {
+  forecast.forEach((day) => {
+    console.log(day);
+    const foreCastDay = document.createElement('h3');
+    foreCastDay.textContent = `${day.date} = ${day.maxTemp}°C`;
+    dataContainer.appendChild(foreCastDay);
+
+    const condition = document.createElement('h5');
+    condition.textContent = `${day.chanceOfRain}% rain chance - ${day.condition}`;
+    dataContainer.appendChild(condition);
+  });
+}
+
+export default function display(location, current, forecast) {
+  clearDisplay();
+  addCityData(location.city, location.country, location.localtime);
+  addCurrentData(
+    current.condition,
+    current.temperature,
+    current.sunrise,
+    current.sunset
+  );
+  addForeCastData(forecast);
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import fetchWeatherData from './processData';
 
 const submit = document.querySelector('button');
 const input = document.querySelector('#city');
+const DEFAULT_CITY = 'Cologne';
 
-submit.addEventListener('click', () => {
-  const weatherData = fetchWeatherData(input.value);
+function kickOffWeatherDisplay(city) {
+  const weatherData = fetchWeatherData(city);
 
   weatherData.then((data) => {
-    console.log(data);
-    display(data);
+    display(data.location, data.current, data.forecast);
   });
+}
+
+submit.addEventListener('click', () => {
+  kickOffWeatherDisplay(input.value);
 });
+
+kickOffWeatherDisplay(DEFAULT_CITY);
